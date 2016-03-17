237d886d899552d4408883803941e08cd2dfd0d2
Mario Huang <suhao_huang@126.com>
2016-03-17 19:21:30 +0800

    add printf

---
+++ dog/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  dog
+//
+//  Created by Mario on 16/3/10.
+//  Copyright © 2016年 Mario. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+//    printf("");
+    return 0;
+}
